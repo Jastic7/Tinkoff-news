ca3e40956271ccd49a70ac6c031efc8cfbe97442
Andrey Morozov <jastic7@yandex.ru>
2018-09-21 16:42:52 +0300

    Add data model for news and news header.

---
+++ TinkoffDemo/TinkoffDemo/News.swift
@@ -0,0 +1,16 @@
+//
+//  News.swift
+//  TinkoffDemo
+//
+//  Created by Andrey Morozov on 21.09.2018.
+//  Copyright © 2018 Jastic7. All rights reserved.
+//
+
+import Foundation
+
+struct News {
+	let header: NewsHeader
+	let content: String
+	let creationDate: Int
+	let modificationDate: Int
+}
---
+++ TinkoffDemo/TinkoffDemo/NewsHeader.swift
@@ -0,0 +1,16 @@
+//
+//  NewsHeader.swift
+//  TinkoffDemo
+//
+//  Created by Andrey Morozov on 21.09.2018.
+//  Copyright © 2018 Jastic7. All rights reserved.
+//
+
+import Foundation
+
+struct NewsHeader {
+	let id: String
+	let text: String
+	let publicationDate: Int
+	let numberOfViews: UInt
+}
