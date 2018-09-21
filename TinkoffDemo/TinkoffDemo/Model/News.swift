4c8bd99ad794dcdaed57072b702efae191a1d570
Andrey Morozov <jastic7@yandex.ru>
2018-09-21 23:13:38 +0300

    Add Codable confirmation for data models.

--- TinkoffDemo/TinkoffDemo/Model/News.swift
+++ TinkoffDemo/TinkoffDemo/Model/News.swift
@@ -8,9 +8,36 @@
 
 import Foundation
 
-struct News {
+struct News: Codable {
 	let header: NewsHeader
 	let content: String
-	let creationDate: Int
-	let modificationDate: Int
+	let creationDate: Date
+	let lastModificationDate: Date
+	
+	enum CodingKeys: String, CodingKey {
+		case header = "title"
+		case content
+		case creationDate
+		case lastModificationDate
+	}
+	
+	enum CreationDateKeys: String, CodingKey {
+		case milliseconds
+	}
+	
+	enum LastModificationDateKeys: String, CodingKey {
+		case milliseconds
+	}
+	
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		header = try container.decode(NewsHeader.self, forKey: .header)
+		content = try container.decode(String.self, forKey: .content)
+		
+		let creationDateContainer = try container.nestedContainer(keyedBy: CreationDateKeys.self, forKey: .creationDate)
+		creationDate = try creationDateContainer.decode(Date.self, forKey: .milliseconds)
+		
+		let modificationDateContainer = try container.nestedContainer(keyedBy: LastModificationDateKeys.self, forKey: .lastModificationDate)
+		lastModificationDate = try modificationDateContainer.decode(Date.self, forKey: .milliseconds)
+	}
 }
--- TinkoffDemo/TinkoffDemo/Model/NewsHeader.swift
+++ TinkoffDemo/TinkoffDemo/Model/NewsHeader.swift
@@ -8,9 +8,30 @@
 
 import Foundation
 
-struct NewsHeader {
+struct NewsHeader: Codable {
 	let id: String
 	let text: String
-	let publicationDate: Int
-	let numberOfViews: UInt
+	let numberOfViews: UInt?
+	let publicationDate: Date
+	
+	enum CodingKeys: String, CodingKey {
+		case id
+		case text
+		case numberOfViews
+		case publicationDate
+	}
+	
+	enum PublicationDateKeys: String, CodingKey {
+		case milliseconds
+	}
+	
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		id = try container.decode(String.self, forKey: .id)
+		text = try container.decode(String.self, forKey: .text)
+		numberOfViews = try container.decodeIfPresent(UInt.self, forKey: .numberOfViews)
+		
+		let publicationDateContainer = try container.nestedContainer(keyedBy: PublicationDateKeys.self, forKey: .publicationDate)
+		publicationDate = try publicationDateContainer.decode(Date.self, forKey: .milliseconds)
+	}
 }
