85ba907b4241bddca4d1c4ccbbd4547a811b2dda
Andrey Morozov <jastic7@yandex.ru>
2018-09-22 10:12:45 +0300

    Add details news view controller.

---
+++ TinkoffDemo/TinkoffDemo/NewsDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NewsDetailsViewController.swift
+//  TinkoffDemo
+//
+//  Created by Andrey Morozov on 22.09.2018.
+//  Copyright © 2018 Jastic7. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailsViewController: UIViewController {
+
+	@IBOutlet weak var numberOfViewsLabel: UILabel!
+	@IBOutlet weak var creationDateLabel: UILabel!
+	@IBOutlet weak var headerLabel: UILabel!
+	@IBOutlet weak var contentLabel: UILabel!
+	
+	var news: News?
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+	func updateDetails(for news: News) {
+		self.news = news
+		
+		numberOfViewsLabel.text = "Watched: \(news.header.numberOfViews ?? 0)"
+		creationDateLabel.text = news.creationDate.description
+		headerLabel.text = news.header.text
+		contentLabel.text = news.content
+	}
+}
