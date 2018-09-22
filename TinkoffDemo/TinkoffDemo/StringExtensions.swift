09b7e44c4e3a38e4dfd5cfe5396aa6db06689654
Andrey Morozov <jastic7@yandex.ru>
2018-09-22 16:38:38 +0300

    Add constraints for news details vc.

--- TinkoffDemo/TinkoffDemo/StringExtensions.swift
+++ TinkoffDemo/TinkoffDemo/StringExtensions.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+extension String {
+	var transformedByHtml: String? {
+		guard let data = data(using: .utf8) else { return nil }
+		do {
+			return try NSAttributedString(data: data,
+										  options: [.documentType: NSAttributedString.DocumentType.html,
+													.characterEncoding: String.Encoding.utf8.rawValue],
+										  documentAttributes: nil).string
+		} catch {
+			return nil
+		}
+	}
+}
