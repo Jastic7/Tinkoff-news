20b82b946097e61a7d364b1e96364fc94f8bd3d7
Andrey Morozov <jastic7@yandex.ru>
2018-09-23 14:24:07 +0300

    Add activityIndicator to news details vc.

--- TinkoffDemo/TinkoffDemo/NewsDetailsViewController.swift
+++ TinkoffDemo/TinkoffDemo/NewsDetailsViewController.swift
@@ -14,19 +14,24 @@ class NewsDetailsViewController: UIViewController {
 	@IBOutlet weak var creationDateLabel: UILabel!
 	@IBOutlet weak var headerLabel: UILabel!
 	@IBOutlet weak var contentLabel: UILabel!
+	@IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 	
-	var news: News?
+	var news: News!
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
+		
+		updateDetails(for: news)
+		activityIndicator.startAnimating()
     }
 
 	func updateDetails(for news: News) {
 		self.news = news
+		activityIndicator.stopAnimating()
 		
-		numberOfViewsLabel.text = "Watched: \(news.header.numberOfViews ?? 0)"
-		creationDateLabel.text = news.creationDate.description
 		headerLabel.text = news.header.text.transformedByHtml
-		contentLabel.text = news.content.transformedByHtml
+		numberOfViewsLabel.text = "Просмотров: \(news.header.numberOfViews)"
+		creationDateLabel.text = news.creationDate?.description
+		contentLabel.text = news.content?.transformedByHtml
 	}
 }
