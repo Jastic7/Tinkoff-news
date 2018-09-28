5d1ca56a8aee35e2d9fa0724f3bd66cf1f6cd6dd
Andrey Morozov <jastic7@yandex.ru>
2018-09-28 22:58:08 +0300

    Update data source for right coredata usage.

--- TinkoffDemo/TinkoffDemo/NewsCoreDataDataSource.swift
+++ TinkoffDemo/TinkoffDemo/NewsCoreDataDataSource.swift
@@ -19,21 +19,22 @@ class NewsCoreDataDataSource: DataSourceProtocol {
 	}
 	
 	func obtainAllEntities() -> [News] {
-		let fetchedNews = persistanceController.find(by: MONews.fetchRequest())
+		let fetchedNews = persistanceController.find(by: MONews.fetchRequest(), in: persistanceController.viewContext)
 		let results = fetchedNews.map { translator.createEntity(from: $0) }
 	
 		return results
 	}
 	
 	func save(entities: [News]) {
-		let context = persistanceController.readContext
-		entities.forEach { news in
-			let predicate = NSPredicate(format: "%K == %@", #keyPath(MONews.header.id), news.header.id)
-			let moNews = persistanceController.findOrCreate(by: MONews.fetchRequest(), with: predicate)
+		let context = persistanceController.privateContext
+		context.perform {
+			entities.forEach { news in
+				let predicate = NSPredicate(format: "%K == %@", #keyPath(MONews.header.id), news.header.id)
+				let moNews = self.persistanceController.findOrCreate(by: MONews.fetchRequest(), with: predicate, in: context)
+				self.translator.fill(entry: moNews, from: news, in: context)
+			}
 			
-			translator.fill(entry: moNews, from: news, in: context)
+			self.persistanceController.save()
 		}
-		
-		persistanceController.save()
 	}
 }
